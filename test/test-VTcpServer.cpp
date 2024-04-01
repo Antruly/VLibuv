565437f589249e1f6bdde120607d5f49d3581f27
unknown <zwy976843568@gmail.com>
2024-04-01 13:20:36 +0800

    update linux build bug

---
+++ test/test-VTcpServer.cpp
@@ -0,0 +1,91 @@
+﻿#pragma once
+#include "VLibuv.h"
+#include <cstdint>
+#include "VTcp.h"
+#include "VTcpServer.h"
+#include "VThread.h"
+#include "VTimer.h"
+#include "VWrite.h"
+
+VTcpServer tcpServer;
+std::string getTestData =
+    "HTTP/1.1 200 OK\r\nContent-Type: text/html; "
+    "charset=utf-8\r\nContent-Length: 123\r\nConnection: "
+    "keep-alive\r\n\r\n<!DOCTYPE html><html><head><title>Sample "
+    "Response</title></head><body><h1>Hello, World!</h1></body></html>";
+std::string getJson =
+    "HTTP/1.1 200 OK\r\nContent-Type: application/json; "
+    "charset=utf-8\r\nContent-Length: 123\r\nConnection: "
+    "keep-alive\r\n\r\n{\"message\": \"Hello, World!\"}";
+int64_t tcpServer_index = 0;
+int64_t tcpServer_maxIndex = 0;
+int64_t tcpServer_readIndex = 0;
+int64_t tcpServer_writeIndex = 0;
+int64_t tcpServer_userMaxIndex = 0;
+
+void tcpServer_close(VTcp* client) {
+  printf("server close !\n");
+}
+
+void tcpClient_close(VTcpClient* client) {
+  tcpServer_index--;
+  printf("client close !\n");
+}
+
+void tcpClient_write(VTcpClient* client, const VBuf* data, int status) {
+  tcpServer_writeIndex++;
+  delete data;
+}
+
+void tcpClient_read(VTcpClient* client, const VBuf* data) {
+  tcpServer_readIndex++;
+  printf("client data:%s\n", data->getData());
+
+  VBuf* newdata = new VBuf(data->getData(), data->size());
+  client->writeData(*newdata);
+}
+
+void newClient(VTcpClient* client) {
+  tcpServer_index++;
+  tcpServer_maxIndex++;
+  if (tcpServer_userMaxIndex < tcpServer_index) {
+    tcpServer_userMaxIndex = tcpServer_index;
+  }
+
+  client->setCloseCb(tcpClient_close);
+  client->setReadCb(tcpClient_read);
+  client->setWriteCb(tcpClient_write);
+  client->clientReadStart();
+  printf("new client connect !\n");
+  client->run();
+}
+
+void timerFunc(VTimer* vtimer) {
+  vtimer->start(timerFunc, 1000, 0);
+}
+
+int main() {
+  tcpServer.setCloseCb(tcpServer_close);
+  tcpServer.setNewClientCb(newClient);
+
+  VThread td;
+  td.start(
+      [&](void* data) {
+        printf("listenIpv4:%s port:8075\n\n", "0.0.0.0");
+        tcpServer.listenIpv4("0.0.0.0", 8075, 0);
+        tcpServer.run();
+      },
+      nullptr);
+
+  while (true) {
+    
+    printf("new client  curruntClient:%lld userMaxIndex:%lld maxClient:%lld\n",
+           tcpServer_index, tcpServer_userMaxIndex, tcpServer_maxIndex);
+    printf("new client  readIndex:%lld writeIndex:%lld\n\n",
+           tcpServer_readIndex, tcpServer_writeIndex);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+  td.join();
+
+  return 0;
+}
